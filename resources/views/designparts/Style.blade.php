a9ba8a1d95f97618f24de37d7cddd67967a47929
vainius99 <vainius99@gmail.com>
2021-10-11 20:39:47 +0300

    create | index dizainai

---
+++ resources/views/designparts/Style.blade.php
@@ -0,0 +1,29 @@
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+    }
+    th {
+        background-color: white;
+    }
+    td {
+        background-color: white;
+    }
+    .yellow:hover {
+    background-color: yellow;
+    }
+    .red:hover {
+    background-color: red;
+    }
+    .green:hover {
+        /* background-color: green;
+         */
+    }
+</style>
